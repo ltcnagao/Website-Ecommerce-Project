b635900331120ce3d4d355221660875f8251fc7a
Leticia <lu.nagao021@gmail.com>
2021-06-09 11:33:29 -0300

    implemented firebase utils, included ability to store authenticated users

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button 
-    className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`} 
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
     {...otherProps}
   >
     {children}
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,19 +3,45 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-        apiKey: "AIzaSyDNp3Rb37bMigS3VnpaaHV8ZJI3dj6ybTc",
-        authDomain: "website-ecommerce-clothing.firebaseapp.com",
-        projectId: "website-ecommerce-clothing",
-        storageBucket: "website-ecommerce-clothing.appspot.com",
-        messagingSenderId: "290801288767",
-        appId: "1:290801288767:web:fd1b7e26b222f9adc19a8a",
-        measurementId: "G-5BSNRD9VYY"
-      };
+  apiKey: "AIzaSyDNp3Rb37bMigS3VnpaaHV8ZJI3dj6ybTc",
+  authDomain: "website-ecommerce-clothing.firebaseapp.com",
+  projectId: "website-ecommerce-clothing",
+  storageBucket: "website-ecommerce-clothing.appspot.com",
+  messagingSenderId: "290801288767",
+  appId: "1:290801288767:web:fd1b7e26b222f9adc19a8a",
+  measurementId: "G-5BSNRD9VYY"
+};
+
+export const createUserProfileDocument = async ( userAuth, additionalData ) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
     
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-export const firestone = firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
