2dafcc6fc4f43b0fe8740c71b90f06923ea42bf6
Leticia <lu.nagao021@gmail.com>
2021-06-26 20:27:23 -0300

    added express server backend stripe

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51J3L6KIIegNpvdSj0OmLGHE7N6UhcSfgMJYG3Vp5xlbrDvUXoi9Bne37e6yBevcZMTrrC5GRlNqESdHzjouMOKRW00lCR9w2vV';
+
+  const onToken = token => {
+    axios({
+      url: 'payment', 
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please sure you use the provided credit cart.'
+      );
+    })
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
